15709691ae3b21d069fea9510018e5a2ce2e284b
ms <schmiddim@gmx.at>
2015-10-16 22:16:25 +0200

    merge

---
+++ arduino-sketches/_04_test_merge_Dht11_ds1820/_04_test_merge_Dht11_ds1820.ino
@@ -0,0 +1,43 @@
+/**
+*Merge _02_test_Dht11 & _01_test_03_ds1820
+* 
+*/
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <dht.h>
+
+#define ONE_WIRE_BUS 3 /* Digitalport D3  (Pin #2) definieren */
+#define dht_dpin A0 //DHT 11
+
+dht DHT;
+OneWire ourWire(ONE_WIRE_BUS); /* Ini oneWire instance */
+DallasTemperature sensors(&ourWire);/* Dallas Temperature Library für Nutzung der oneWire Library vorbereiten */
+
+
+void setup(){
+  Serial.begin(9600);  
+  delay(300);//Let system settle
+  Serial.println("Setup");
+  sensors.begin();/* Initialize  Dallas Temperature library */
+  sensors.setResolution(TEMP_12_BIT); // Set precision  12-Bit
+  delay(700);//Wait rest of 1000ms recommended delay before
+  //accessing sensor
+}//end "setup()"
+
+void loop(){
+
+    DHT.read11(dht_dpin);  //Read from DHT11
+    sensors.requestTemperatures(); // Read from DS1820
+
+    Serial.print("DHT 11 Current humidity = ");
+    Serial.print(DHT.humidity);
+    Serial.println("%  ");
+    Serial.print("DHT 11 temperature = ");
+    Serial.print(DHT.temperature); 
+    Serial.println("C  ");
+    Serial.print("DS1820 temperature = ");
+    Serial.print(sensors.getTempCByIndex(0) );
+    Serial.println("C ");
+    delay(800);//Don't try to access too frequently... in theory
+}// end loop()
